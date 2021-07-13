cfb9811a646f76248718364621399ede086c7265
AMDraghici <madraghi99@gmail.com>
2021-07-12 23:25:53 -0400

    re-organize cjs standard

--- Scripts/11_cjs_mod_standard.R
+++ Scripts/11_cjs_mod_standard.R
@@ -5,16 +5,19 @@ model{
   # CJS Likelihood -----------------------------------------------------------------------------------
   for(i in 1:n){
     for(t in (initial_entry[i]+1):k){
-      
-      phi[i,t] <- equals(female[i], 1) * phiF + equals(female[i], 0)* phiM
-      p[i, t] <- equals(female[i], 1)* pF + equals(female[i], 0)*pM
-      
-      a[i,t] ~ dbern(phi[i,t] * a[i,t-1])
-      x[i,t] ~ dbern(p[i, t] * a[i,t])
+      x[i,t] ~ dbern(p[i] * a[i,t])
+      a[i,t] ~ dbern(phi[i] * a[i,t-1])
     }
+    
   }
   
   # Priors--------------------------------------------------------------------------------------------
+  
+  for(i in 1:n){
+    p[i] <- equals(female[i], 1)* pF + equals(female[i], 0)* pM
+    phi[i] <-  equals(female[i], 1) * phiF + equals(female[i], 0)* phiM
+  }
+  
   
   # Survival by sex
   phiF ~ dbeta(1,1)
