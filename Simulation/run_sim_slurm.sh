01bdc916936f60250d4f4dd2eb204f14085b9090
AMDraghici <madraghi99@gmail.com>
2021-12-09 00:24:49 -0500

    update simulation study

---
+++ Simulation/run_sim_slurm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --array=1-12
+#SBATCH --time=00:10:00
+#SBATCH --mem=5G
+#SBATCH --error=Logs/rep_%A_%a_long.log
+#SBATCH --output=Logs/rep_%A_%a_long.log
+
+module load jags
+module load r/4.1.2
+
+Rscript --vanilla run_test.R $SLURM_ARRAY_TASK_ID param_grid.rds &> Logs/long_run_${SLURM_ARRAY_TASK_ID}.Rout
