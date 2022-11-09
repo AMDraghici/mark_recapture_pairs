0fe24f62f6097e5718191f6dc5f4b9ea98871f68
AMDraghici <madraghi99@gmail.com>
2022-11-09 10:47:22 -0500

    add recapture conditioning

--- Scripts/optim_approach.R
+++ Scripts/optim_approach.R
@@ -16,7 +16,7 @@ src_dir <- getwd()#"/home/sbonner/Students/Statistics/A_Draghici/Research/mark_r
 # source(file.path(src_dir,"Scripts","jolly_seber_mod_nimble.R"))
 # source(file.path(src_dir,"Scripts","pair_swap_mod_nimble9.R"))
 source(file.path(src_dir,"Scripts","cormack_jolly_seber_mod_nimble.R"))
-source(file.path(src_dir,"Scripts","fn_sim_pair_data4.R"))
+source(file.path(src_dir,"Scripts","fn_sim_pair_data3.R"))
 # source(file.path(src_dir,"Scripts","fn_process_hduck_data.R"))
 
 recapture_correlations <- list()
@@ -25,7 +25,7 @@ for(i in 1:1000){
   # TESTING SIMULATED DATA METHOD -------------------------------------------------------------------------------------------------
   # Set number of occasions and animals
   k = 30
-  n = 350
+  n = 400
   
   # Seeds for Testing
   # set.seed(pi)
@@ -315,9 +315,35 @@ compute_surv_cor <- function(x, PMF, PHIF, PHIM){
 }
 
 
-y <- compute_surv_cor(seq(0,1,by = 0.01),0.65625, 0.8, 0.8)
-plot(y = y , x =seq(0,1,by = 0.01), type = "l" )
+# TESTING SIMULATED DATA METHOD -------------------------------------------------------------------------------------------------
+# Set number of occasions and animals
+k = 30
+n = 400
 
+# Seeds for Testing
+# set.seed(pi)
+# set.seed(1e5)
+
+# Parameter Grid 
+param_list <- list(
+  n            = n, # Number of Animals
+  k            = k, # Occasions
+  prop.female  = 0.5, # Proportion of simulated individuals to be female
+  delta        = rep(1, k), # Probability that mating is attempted
+  phi.f        = rep(0.8, k), # Marginal Prob of Female Survival
+  phi.m        = rep(0.8, k), # Marginal Prob of Male Survival
+  gam          = rep(0, k), # Correlation in Survival Prob of Mates
+  p.f          = rep(0.75, k), # Marginal Prob of Female Recapture
+  p.m          = rep(0.75, k), # Marginal Prob of Male Recapture
+  rho          = rep(0.5, k), # Correlation in male survival rates
+  betas        = list(beta0 = 1e3, beta1 = 0), # inv.logit(Beta0 + Beta1 * hij) = Prob of reforming a pair from t-1 after hij times together
+  rand_init    = F, # Randomize Initial Entry (just leave as F)
+  init         = sample(1, n, TRUE), # Initial Entry into population for individual n
+  show_unmated = T # Include unmated observations in attempt to mate step
+)
+
+# Generate One set of Data
+ps_data <- sim_dat(param_list) # 
 
 
 N <- 0
@@ -326,25 +352,41 @@ apairs_f <- ps_data$apairs_f
 nm <- ps_data$nm
 nf <- ps_data$nf
 k <- ps_data$k
+first_capture_f <- ps_data$first_capture_f
+recap_f <- ps_data$recap_f
+recap_m <- ps_data$recap_m
 
 for(i in 1:nf){
-  for(j in 2:k){
+  for(j in (first_capture_f[i]+1):k){
     if(!is.na(apairs_f[i,j-1])){
       
       if(apairs_f[i,j-1] == (nm+1)) next
-      
+    
       if(!is.na(apairs_f[i,j])){
         if(apairs_f[i,j] == apairs_f[i,j-1]){
-          n <- n + 1
-          N <- N +1
+          if(recap_f[i,j] == 1 & recap_m[apairs_f[i,j],j] == 1){
+            n <- n + 1
+            N <- N +1
+          } else {
+            N <- N+1
+          } 
         } else {
           N <- N + 1
         }
-      } else if(apairs_f[i,j-1] != (nm+1)){
+      } else {
         N <- N + 1
       }
     }
   }
 }
 
-compute_surv_cor(n/N,0.65625, 0.8, 0.8)
+obs <- compute_surv_cor(n/N,0.65625, 0.8, 0.8)
+
+
+
+y <- compute_surv_cor(seq(0,1,by = 0.01),0.65625, 0.8, 0.8)
+plot(y = y , x =seq(0,1,by = 0.01), type = "l" )
+abline(v = qbinom(c(0.05,0.95),N, 0.8*0.8 * 0.65625)/N, col = "red")
+abline(v = n/N, col = "blue")
+abline(h = obs, col = "blue")
+
